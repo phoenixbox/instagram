e99ecd98e86581a2c284e84033b41798b34c2f41
Shane Rogers <rogerssh@tcd.ie>
2013-05-07 20:09:14 -0600

    initial commits

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instagram::Application.config.secret_token = 'c06edc97329a12a0543b9e6603a88fd503b03faef5b572183479a37acf5f8a1eb9f7ec4c13aef271d0058b3adef299fcd42f54a40d053896dafabb098715376a'
